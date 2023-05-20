e1ef31ce0235c573e73dd1604d1d4eab4101cff7
Justin Cho <justincho1290@gmail.com>
2023-05-20 15:50:03 -0700

    Initial Commit

---
+++ Hackathon Clothes Trading/Hackathon_Clothes_TradingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon_Clothes_TradingApp.swift
+//  Hackathon Clothes Trading
+//
+//  Created by Justin Cho on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon_Clothes_TradingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
